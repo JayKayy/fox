12e723e573d462c15217e5f78e587810e2ce3f94
John Kwiatkoski <jkwiatkoski@protonmail.com>
2019-12-06 12:31:03 -0500

    some key input working

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/MarinX/keylogger"
+	"github.com/micmonay/keybd_event"
 	"github.com/sirupsen/logrus"
+	//	"runtime"
 	"strings"
+	//	"time"
 )
 
 type Expansion struct {
@@ -14,9 +17,23 @@ type Expansion struct {
 
 func main() {
 
+	// Prep for send keys
+
+	kb, err := keybd_event.NewKeyBonding()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Ignoring this warning from micmonay, YOLO!
+	// We wont be sending keys immediately
+	// For linux, it is very important wait 2 seconds
+	//if runtime.GOOS == "linux" {
+	//	time.Sleep(2 * time.Second)
+	//}
+
 	// find keyboard device, does not require a root permission
 	keyboard := keylogger.FindKeyboardDevice()
-
 	// check if we found a path to keyboard
 	if len(keyboard) <= 0 {
 		logrus.Error("No keyboard found...you will need to provide manual input path")
@@ -44,6 +61,8 @@ func main() {
 
 	// Set static expansions
 	expansions = append(expansions, Expansion{";;,", "Hi there!\n Thanks for contacting DigitalOcean!"})
+	expansions = append(expansions, Expansion{";;.", "Hi there!\n Thank you for your response!"})
+	expansions = append(expansions, Expansion{";;/", "Let me know if you have any further questions.\n\nRegards,\n\nJohn Kwiatkoski\nSenior Developer Support Engineer - Kubernetes"})
 
 	var pressed = make([]string, 0, 50)
 	for e := range events {
@@ -56,23 +75,15 @@ func main() {
 			// if the state of key is pressed
 			if e.KeyPress() {
 				if len(pressed) == cap(pressed) {
-					// TODO I want the new array to begin with 40,41,42,43...
-					// zero out 0-39
-
-					for i := 0; i < 40; i++ {
-						pressed[i] = ""
-					}
-					fmt.Println("zeroed out")
-					temp := make([]string, 0, 50)
-					fmt.Printf("before copy pressed[40:] is: %s", pressed[40:])
-					n := copy(temp, pressed[40:])
-					fmt.Printf("%d copied. temp is now: %s", n, temp)
-					pressed = temp
-					fmt.Println(pressed)
+					pressed = reset(pressed)
 				}
 				logrus.Println("[event] press key ", e.KeyString())
 				pressed = append(pressed, e.KeyString())
-				pressed = checkExpand(pressed, expansions)
+				match, exp := checkExpand(pressed, expansions)
+				if match {
+					expand(exp, kb)
+					pressed = make([]string, 0, 50)
+				}
 				fmt.Println(pressed)
 
 			}
@@ -87,7 +98,7 @@ func main() {
 		}
 	}
 }
-func checkExpand(pressed []string, expansions []Expansion) []string {
+func checkExpand(pressed []string, expansions []Expansion) (bool, Expansion) {
 
 	// compact pressed to a single string
 	// check whether any abbrieviations (abbrevs)
@@ -96,8 +107,31 @@ func checkExpand(pressed []string, expansions []Expansion) []string {
 	joined := strings.Join(pressed, "")
 	for _, exp := range expansions {
 		if strings.Contains(joined, exp.abbrev) {
-			fmt.Printf("Match found in %s!", exp.abbrev)
+			fmt.Printf("Match found in %s! Expanding...", exp.abbrev)
+			return true, exp
 		}
 	}
-	return pressed
+	// Not sure how to best handle the return of this function
+	return false, Expansion{"", ""}
+}
+
+func expand(exp Expansion, kb keybd_event.KeyBonding) {
+
+	//Built expansion here
+	kb.SetKeys(keybd_event.VK_A, keybd_event.VK_B)
+	//launch
+	err := kb.Launching()
+	if err != nil {
+		panic(err)
+	}
+	//Ouput : AB
+}
+func reset(current []string) []string {
+
+	// 40 is arbitrary here and may need to move if
+	// the slice is ever made bigger
+	fmt.Println("keys reset.")
+	temp := make([]string, 0, 50)
+	temp = append(temp, current[40:]...)
+	return temp
 }
